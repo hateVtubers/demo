a29b17eff891ae3daaa5c66438fa99ed126cf8e5
hate-vtubers <gmolloflores@gmail.com>
2022-01-08 09:41:50 -0300

    fixed: bug of types props & an client error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { GetServerSideProps, NextPage } from "next";
+import type { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next";
 import { User, getAuthCookieProps } from "next-firebase-auth-cookies";
 import { providers } from "firebase.config";
 import { LoginButtons } from "components/loginButtons";
@@ -6,10 +6,7 @@ import useSWR from "swr";
 import { List } from "components/List";
 import Link from "next/link";
 
-type ProviderDataValue = string | null;
-
-// @ts-ignore
-const Home: NextPage = ({ user }: { user: any }) => {
+const Home: NextPage = ({ user }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const { data } = useSWR<User>("/api/auth/login");
 
   return (
@@ -19,15 +16,6 @@ const Home: NextPage = ({ user }: { user: any }) => {
           <h1 className="font-medium text-lg text-center mb-4">
             {"Hi you're loged"}
           </h1>
-          <List customClassName="flex-col gap-2">
-            {Object.entries<ProviderDataValue>(
-              data?.providerData[0] ?? user?.providerData[0]
-            ).map(([key, value]) => (
-              <li key={key}>
-                <p>{`${key.toUpperCase()}: ${value}`}</p>
-              </li>
-            ))}
-          </List>
         </>
       ) : (
         <>
@@ -45,7 +33,10 @@ const Home: NextPage = ({ user }: { user: any }) => {
       )}
       <footer className="absolute bottom-5 left-0 right-0">
         <Link href={"/api/auth/login"}>
-          <a className="text-center block hover:text-neutral-500 transition-colors" target={"_blank"}>
+          <a
+            className="text-center block hover:text-neutral-500 transition-colors"
+            target={"_blank"}
+          >
             {`view json in "api/auth/[...login]"`}
           </a>
         </Link>
@@ -55,7 +46,7 @@ const Home: NextPage = ({ user }: { user: any }) => {
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  const user = await getAuthCookieProps(req, res);
+  const user: User = await getAuthCookieProps(req, res);
   return {
     props: {
       user,
