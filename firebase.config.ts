d2014d9bcd644da5a9df9e2bda562a12d8d5d95a
hate-vtubers <gmolloflores@gmail.com>
2022-01-08 06:13:00 -0300

    hidden firebase config

--- firebase.config.ts
+++ firebase.config.ts
@@ -7,12 +7,12 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC6N-xXuxKUZCtnLT5f9nIFbZ_6jKenlwA",
-  authDomain: "package-demo-4e3bf.firebaseapp.com",
-  projectId: "package-demo-4e3bf",
-  storageBucket: "package-demo-4e3bf.appspot.com",
-  messagingSenderId: "309254638096",
-  appId: "1:309254638096:web:12058a1f72dcf8000c9895",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
