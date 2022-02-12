ab25ba8f2f547c78eb4b1235ebc7995027f0109a
hateVtubers <gmolloflores@gmail.com>
2022-02-12 03:03:35 -0300

    remove apis

--- pages/api/auth/[...login].ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { getAuthCookieApi } from "next-firebase-auth-cookies";
-
-const loginEndpoint = (req: NextApiRequest, res: NextApiResponse) => {
-  getAuthCookieApi({ req, res });
-};
-
-export default loginEndpoint;
